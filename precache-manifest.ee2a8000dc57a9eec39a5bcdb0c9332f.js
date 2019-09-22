1d2c1358842899ffe77724c595b59e09a21d7fe2
Alejandro U. Alvarez <aluralma@gmail.com>
2019-09-22 20:16:46 +0100

    Updates

---
+++ precache-manifest.ee2a8000dc57a9eec39a5bcdb0c9332f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2663a3216be16b7fadc3a17331a7bfb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c274e7e65ac4e6e99d1",
+    "url": "/static/css/main.fb1cc8b5.chunk.css"
+  },
+  {
+    "revision": "45305cd0b8461d8808a6",
+    "url": "/static/js/2.5c9f1905.chunk.js"
+  },
+  {
+    "revision": "6c274e7e65ac4e6e99d1",
+    "url": "/static/js/main.b3829972.chunk.js"
+  },
+  {
+    "revision": "9422dee6af38b59510b4",
+    "url": "/static/js/runtime-main.d93989b0.js"
+  },
+  {
+    "revision": "50944d934c34107a556a41d6c940c9f6",
+    "url": "/static/media/logo.50944d93.png"
+  }
+]);
