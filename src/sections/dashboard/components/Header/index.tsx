0786ffe90d6fd77b06095ee0a4e01460619b7fe8
Alejandro U. Alvarez <aluralma@gmail.com>
2019-09-23 10:57:25 +0100

    split styles in per-component files

---
+++ src/sections/dashboard/components/Header/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Row, Col, Navbar, Nav } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import './index.scss';
+
+type Props = {
+};
+
+export default (props: Props) => (
+  <Row className='header'>
+    <Col>
+      <Navbar bg="transparent" variant="dark">
+        <Navbar.Brand href="/dashboard">Dashboard</Navbar.Brand>
+        <Navbar.Collapse className="justify-content-end">
+          <Nav.Link href="/logout">
+            <FontAwesomeIcon icon="question-circle" /> Help
+          </Nav.Link>
+          <Nav.Link href="/logout">
+            <FontAwesomeIcon icon="sign-out-alt" /> Logout
+          </Nav.Link>
+        </Navbar.Collapse>
+      </Navbar>
+    </Col>
+  </Row>
+);
