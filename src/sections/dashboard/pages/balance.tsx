9d5bef771304969ff24373afb5c60095b5b738df
Alejandro U. Alvarez <aluralma@gmail.com>
2019-09-23 14:18:09 +0100

    add placeholder balance history chart

--- src/sections/dashboard/pages/balance.tsx
+++ src/sections/dashboard/pages/balance.tsx
@@ -1,9 +1,72 @@
 import React from "react";
-import { Row, Col, Form, Button } from "react-bootstrap";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Row, Col } from "react-bootstrap";
 import Helmet from "react-helmet";
+import {
+  ResponsiveContainer,
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ReferenceLine
+} from "recharts";
 
 import Header from "../components/Header";
+
+const sampleData = [
+  {
+    name: "Jan",
+    2018: 40,
+    2019: 14,
+  },
+  {
+    name: "Feb",
+    2018: 45,
+    2019: 24,
+  },
+  {
+    name: "Mar",
+    2018: 60,
+    2019: 98,
+  },
+  {
+    name: "May",
+    2018: 10,
+    2019: 15,
+  },
+  {
+    name: "Jun",
+    2018: 18.9,
+    2019: 48,
+  },
+  {
+    name: "Jul",
+    2018: 23.9,
+    2019: 58,
+  },
+  {
+    name: "Aug",
+    2018: 34.9,
+    2019: 70,
+  },
+  {
+    name: "Sep",
+    2018: 5,
+    2019: 10,
+  },
+  {
+    name: "Oct",
+    2018: 14,
+    2019: 0,
+  },
+  {
+    name: "Nov",
+    2018: 34.9,
+    2019: 0,
+  },
+];
 
 export default () => {
   return (
@@ -18,26 +81,34 @@ export default () => {
         <Col>
           <div className="dashboard-widget">
             <h2>Balance history</h2>
-            <Form>
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
-
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-              <Form.Group controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-              </Form.Group>
-              <Button variant="primary" type="submit">
-                <FontAwesomeIcon icon='save'/> Save changes
-              </Button>
-            </Form>
+            <ResponsiveContainer width='100%' height={300}>
+              <LineChart
+                data={sampleData}
+                margin={{
+                  top: 5, right: 30, left: 20, bottom: 5,
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <ReferenceLine x="May" stroke="#512c62" />
+                <ReferenceLine x="Sep" stroke="#512c62" />
+                <Tooltip />
+                <Legend />
+                <Line
+                  type="linear"
+                  dataKey="2018"
+                  stroke="#f75f00"
+                  animationDuration={500}
+                />
+                <Line
+                  type="linear"
+                  dataKey="2019"
+                  stroke="#c93838"
+                  animationDuration={500}
+                />
+              </LineChart>
+            </ResponsiveContainer>
           </div>
         </Col>
       </Row>
