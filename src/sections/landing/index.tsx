f0ce930b2156cea6f9e7c1bcd032724f2ed12342
Alejandro U. Alvarez <aluralma@gmail.com>
2019-09-23 20:28:52 +0100

    improve wording and layout of the landing page

--- src/sections/landing/index.tsx
+++ src/sections/landing/index.tsx
@@ -6,12 +6,12 @@ import Navbar from "components/Navbar";
 import Footer from "components/Footer";
 
 import "./index.scss";
-import faq from './faq';
+import faq from "./faq";
 
 export default () => {
   return (
     <>
-      <header className='landing'>
+      <header className="landing">
         <div className="bg-angle" />
         <section>
           <Navbar />
@@ -20,49 +20,69 @@ export default () => {
               <Col>
                 <div className="hero">
                   <h2>Tip the Web</h2>
-                  <p>
-                    Our mission is to move the web towards a future where
-                    creators and publishers can get paid for their work{" "}
-                    <b>without</b> relying on ads, paywalls, or the abuse of
-                    personal data.
-                  </p>
-                  <h3>Get started</h3>
-                  <p>
-                    <Button variant='secondary' href="/dashboard">Internet user</Button>
-                    <Button variant='secondary' href="/dashboard">Content creator</Button>
-                  </p>
-                  <small className="text-muted">
-                    - You can definitely be both!
-                  </small>
+                  <p>Support content creators on the web directly.</p>
                 </div>
               </Col>
             </Row>
           </Container>
         </section>
       </header>
-      <section className='content'>
-          <Container>
-            <h3 style={{ marginBottom: '1.5em' }}>Frequently Asked Questions</h3>
-            <Accordion className='simple'>
-              {faq.map((q, index) => (
-                <Card key={q.question}>
-                  <Accordion.Toggle as={Card.Header} eventKey={String(index)} >
-                    {q.question}
-                    <div style={{ float: 'right' }}>
-                      <FontAwesomeIcon icon='angle-right' />
-                    </div>
-                  </Accordion.Toggle>
-                  <Accordion.Collapse eventKey={String(index)}>
-                    <Card.Body>
-                      {q.answer}
-                    </Card.Body>
-                  </Accordion.Collapse>
-                </Card>
-              ))}
-            </Accordion>
-          </Container>
-        </section>
-        <Footer />
+      <section className="content">
+        <Container>
+          <Row className='mb-5'>
+            <Col>
+              <h3 className="small">Frequently Asked Questions</h3>
+              <Accordion className="simple">
+                {faq.map((q, index) => (
+                  <Card key={q.question}>
+                    <Accordion.Toggle as={Card.Header} eventKey={String(index)}>
+                      {q.question}
+                      <div style={{ float: "right" }}>
+                        <FontAwesomeIcon icon="angle-right" />
+                      </div>
+                    </Accordion.Toggle>
+                    <Accordion.Collapse eventKey={String(index)}>
+                      <Card.Body>{q.answer}</Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                ))}
+              </Accordion>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="get-started mt-5">
+              <Row className="justify-content-md-center">
+                <Col md={ 5 } className='border-right pr-3'>
+                  <h3>Are you a Content Creator?</h3>
+                  <p>Get paid by your users for your content!</p>
+                  <p>
+                    Earn more for content you publish on your website, or
+                    platforms such as YouTube, Twitch, Github...
+                  </p>
+                  <div className="text-center">
+                    <Button variant="secondary" className='rounded-pill' href="/dashboard">
+                      Sign Up
+                    </Button>
+                  </div>
+                </Col>
+                <Col md={ 5 } className='pl-5'>
+                  <h3>Are you a user?</h3>
+                  <p>
+                    Support your favourite content creators on the web directly
+                    easily!
+                  </p>
+                  <div className="text-center">
+                    <Button variant="secondary" className='rounded-pill' href="/dashboard">
+                      Download extension
+                    </Button>
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <Footer />
     </>
   );
 };
